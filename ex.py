fce14e6e2855277b6c5467cd81ff6a3d29976cfe
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-31 15:18:36 -0400

    ch8

--- ex.py
+++ ex.py
@@ -18,13 +18,13 @@ print(s3.cat.ordered)
 
 # Ch6
 # Add a numeric series to itself
-first= pd.Series([1, 5, 10, 12], dtype='int8')
+first = pd.Series([1, 5, 10, 12], dtype='int8')
 print(f'Adding the series to itself \n{first + first}')
 print(f'Using the add method on the series to add to itself \n{first.add(first)}')
 print(f'Show the series unadulterated: \n{first}')
 
 # Ch7
-another = pd.Series([1, 5, 10, 12,5, None])
+another = pd.Series([1, 5, 10, 12, 5, None])
 print(f'count of non null entries: {another.count()}')
 print(f'total count of items (including Nones) {another.size}')
 print(f'total unique count of items (excludes Nones): {another.nunique()}')
@@ -33,3 +33,21 @@ print(f'find the max of the series: {another.max()}')
 print(f'use agg method to find all of the above: \n{another.agg(["mean", "max"])}')
 
 # Ch8
+ch8 = pd.Series([1, 5, 10, 12, 5], dtype="Int64")
+print(f'memory size of ch8: {ch8.nbytes}')
+convert = ch8.astype("uint8")
+print(f'memory size of convert: {convert.nbytes}')
+
+print(f'converting uint8: \n{convert}')
+print(f'printing out int64 \n{np.iinfo("int64")}')
+print(f'printing out uint8 \n{np.iinfo("uint8")}')
+print(f'printing out float16 \n{np.finfo("float16")}')
+print(f'printing out float64 \n{np.finfo("float64")}')
+
+ch8_string = pd.Series(["one string", "two string", "red string", "blue string","one string", "two string", "red string", "blue string","one string", "two string", "red string", "blue string"])
+print(f'memory size of ch8_string: {ch8_string.nbytes}')
+print(f'memory size of ch8_string_convert: {ch8_string.astype("category").nbytes}')
+
+city_type = pd.CategoricalDtype(categories=pd.Series(sorted(set(ch8_string))), ordered=True)
+print(f'memory size of ch8_string_convert with order: {ch8_string.astype(city_type).nbytes}')
+
