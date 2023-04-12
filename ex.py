bc421ac8c19aec21feeedc14e4d1ba02be0ef465
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-04-11 21:42:34 -0400

    working on exercises from ch9

--- ex.py
+++ ex.py
@@ -44,10 +44,27 @@ print(f'printing out uint8 \n{np.iinfo("uint8")}')
 print(f'printing out float16 \n{np.finfo("float16")}')
 print(f'printing out float64 \n{np.finfo("float64")}')
 
-ch8_string = pd.Series(["one string", "two string", "red string", "blue string","one string", "two string", "red string", "blue string","one string", "two string", "red string", "blue string"])
+ch8_string = pd.Series(
+    ["one string", "two string", "red string", "blue string", "one string", "two string", "red string", "blue string",
+     "one string", "two string", "red string", "blue string"])
 print(f'memory size of ch8_string: {ch8_string.nbytes}')
 print(f'memory size of ch8_string_convert: {ch8_string.astype("category").nbytes}')
 
 city_type = pd.CategoricalDtype(categories=pd.Series(sorted(set(ch8_string))), ordered=True)
 print(f'memory size of ch8_string_convert with order: {ch8_string.astype(city_type).nbytes}')
 
+# Ch9
+# Create series from numeric column that has the value of 'high' if it is equal
+# to or above the mean and 'low' if it is below the mean using .apply()
+the_series = pd.Series([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25])
+
+
+def displayHigh(val):
+    if (val >= the_series.mean()):
+        return 'High'
+    return 'Low'
+
+
+print(f'Display High if val is gte mean, display Low if val is lt mean, using apply \n{the_series.apply(displayHigh)}')
+print(
+    f'Display High if val is gte mean, display Low if val is lt mean, using select/where \n{the_series.where(lambda x: x < the_series.mean(), "High")}')
